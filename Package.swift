43123a09184808e2c8d3cbdba950682b4229c6ae
Maurice Kraus <maurice.kraus@stud.tu-darmstadt.de>
2020-03-06 09:15:49 +0100

    This removes unnecessary comments

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         .tvOS(.v10),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RxAnimated",
             targets: ["RxAnimated"]),
@@ -20,8 +19,6 @@ let package = Package(
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxAnimated",
             dependencies: [
